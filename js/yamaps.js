e621d2446dd73cad9dacbe841da91ccf576aa9e1
MasGS <george.sergeevich@gmail.com>
2019-11-30 21:21:09 +0300

    ...

--- js/yamaps.js
+++ js/yamaps.js
@@ -3,4 +3,13 @@ ymaps.ready(function () {
 			  center: [55.76, 37.64],
 			  zoom: 10
 			});
+
+                        // Создает метку и добавляет ее на карту
+                        var placemark = new YMaps.Placemark(new YMaps.GeoPoint(37.64, 55.76));
+                        placemark.name = "Салон красоты";
+                        placemark.description = "творим искусство";
+                        map.addOverlay(placemark);
+
+                        // Открывает балун
+                        placemark.openBalloon();
 		  });
